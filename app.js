413c0dd004ac910d755ac736399b35c393ce7ce8
Roger Trevisan <roger.b.trevisan@gmail.com>
2020-04-29 22:49:53 -0300

    chamando e configurando bodyparser(require and configurate bodyparser)

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 //Carregando módulos
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require("body-parser")
 const app = express
 //Configurações
+    //Body parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
     //Handlebars
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine','handlebars');
@@ -10,3 +14,4 @@ const app = express
 const PORTA = 8181
 app.listen(PORTA,() => {
     console.log(`Servidor rodando na porta: ${PORTA}`)
+    })
