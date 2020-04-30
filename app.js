60e26c47f2b2b0ed568bd2011af9f6ba2be7b2c1
Roger Trevisan <roger.b.trevisan@gmail.com>
2020-04-29 23:03:53 -0300

    instalando express(install express)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Carregando módulos
 const express = require('express')
-const handlebars = require('express-handlebars')
+const handlebars = require("express-handlebars")
 const bodyParser = require("body-parser")
 const app = express
 //Configurações
