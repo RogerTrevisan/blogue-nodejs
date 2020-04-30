d412540b5c2ee115138aec0a53477f21e43c451b
Roger Trevisan <roger.b.trevisan@gmail.com>
2020-04-29 22:39:00 -0300

    requerindo handlebar se definindo o main(require  the handlebars and  define the main)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //Carregando módulos
-    const express = require('express')
-    const app = express
-
+const express = require('express')
+const handlebars = require('express-handlebars')
+const app = express
+//Configurações
+    //Handlebars
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine','handlebars');
 //Conexão express
 const PORTA = 8181
 app.listen(PORTA,() => {
     console.log(`Servidor rodando na porta: ${PORTA}`)
-})
