9f91c9d2b84b77746cac999d365a3563b6f45fbf
Roger Trevisan <roger.b.trevisan@gmail.com>
2020-05-01 22:15:15 -0300

    criando conexão com o model e uma rota post

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
 router.get('/',(req,res) => {
     res.render('admin/index')
 })
@@ -10,7 +12,17 @@ router.get('/categorias',(req,res) => {
 router.get('/categorias/add', (req,res) => {
     res.render('admin/addcategorias')
 })
-
+router.post('/categorias/nova',(req,res) => {
+    const novaCategoria = {
+        nome: req.body.name,
+        slug: req.body.slug
+    }
+    new Categoria(novaCategoria).save().then(() => {
+        console.log('Nova categoria adicionada com sucesso.')
+    }).catch((err) => {
+        console.log('Erro ao adicionar nova tabela.')
+    })
+})
 router.get('/posts',(req,res) => {
     res.send('Página posts')    
 })
