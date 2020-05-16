5d004a38e76247d72da4c2bdb28411d78df304f5
Roger Bulhões Trevisan da Silva <roger.b.trevisan@gmail.com>
2020-05-16 13:48:43 -0300

    modulo validaPostagem

---
+++ control/validaPostagem.js
@@ -0,0 +1,57 @@
+let validaPostagem = function validarPostagem(dados) {
+    var erros = []
+        
+    if (!dados.titulo || typeof dados.titulo == undefined || dados.titulo == null) {
+        erros.push({texto: 'Preencha o campo nome'})
+    }
+    
+    if (!dados.slug || typeof dados.slug == undefined || dados.slug == null) {
+        erros.push({texto: 'Preencha o campo slug'})
+    }
+
+    if (dados.slug.length < 3) {
+        erros.push({
+            texto: 'O nome do Slug é muito curto, nescessário ter ao menos 3 digitos.'
+        })
+    }
+
+    if (/\s/g.test(dados.slug) == true) {
+        erros.push({
+            texto: 'O slug não pode conter espaço vazio.'
+        })
+    }
+    
+    if (/[A-Z]/g.test(dados.slug) == true) {
+        erros.push({
+            texto: 'O slug não pode conter letras maiúsculas.'
+        })
+    }
+    
+    if (/[0-9]/g.test(dados.slug) == true) {
+        erros.push({
+            texto: 'O slug não pode conter numeros.'
+        })
+    }
+    
+    if (/[-_=+§~^´`:;/?.,><)(*&¬¨$#@!"'¹²³£¢|]/g.test(dados.slug) == true) {
+        erros.push({
+            texto: 'O slug não pode conter caracteres especiais.'
+        })
+    }
+    
+    if (!dados.descricao || typeof dados.descricao == undefined || dados.slug == null ) {
+        erros.push({
+            texto: 'Preencha o campo de descrição'
+        })
+    }
+
+    if (dados.categoria == '0') {
+        erros.push({
+            texto: 'É preciso selecionar uma categoria'
+        })
+    }
+
+   return erros
+}
+
+module.exports = validaPostagem
