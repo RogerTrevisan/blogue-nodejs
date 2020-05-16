5c335e110c5845e53344a415bf152b90caf5fadc
Roger Bulhões Trevisan da Silva <roger.b.trevisan@gmail.com>
2020-05-16 13:51:31 -0300

    excluindo validação de linha e importando modulos

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ require('../models/Categoria')
 require('../models/Postagem')
 const Categoria = mongoose.model('categorias')
 const Postagem = mongoose.model('postagens')
+const validaCategoria = require('../control/validaCategoria.js')
+const validaPostagem = require('../control/validaCategoria.js')
 
 router.get('/',(req,res) => {
     res.render('admin/index')
@@ -24,48 +26,8 @@ router.get('/categorias/add', (req,res) => {
 })
 
 router.post('/categorias/nova',(req,res) => {
-    var erros = []
-    let name = req.body.name
-    let slug = req.body.slug
+    var erros = validaCategoria(req.body)
         
-    if (!name || typeof name == undefined || name == null) {
-        erros.push({texto: 'Nome inválido'})
-    }
-
-    if (!slug || typeof slug == undefined || slug == null) {
-        erros.push({texto: 'Slug inválido'})
-    }
-
-    if (slug.length < 3) {
-        erros.push({
-            texto: 'O nome do Slug é muito curto, nescessário ter ao menos 3 digitos.'
-        })
-    }
-
-    if (/\s/g.test(slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter espaço vazio.'
-        })
-    }
-    
-    if (/[A-Z]/g.test(slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter letras maiúsculas.'
-        })
-    }
-    
-    if (/[0-9]/g.test(slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter numeros.'
-        })
-    }
-    
-    if (/[-_=+§~^´`:;/?.,><)(*&¬¨$#@!"'¹²³£¢|]/g.test(slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter caracteres especiais.'
-        })
-    }
-    
     if (erros.length > 0 ) {
         res.render('admin/addcategorias', {erros: erros})
     }else{
@@ -84,102 +46,65 @@ router.post('/categorias/nova',(req,res) => {
 })
 
 router.get('/categorias/edit/:id', (req,res) => {
-
+    
     Categoria.findOne({_id:req.params.id}).lean().then((categoria) => {
         res.render('admin/editcategorias', {categoria:categoria})
-     }).catch((err) =>  {
-         req.flash('error_msg', 'Categotia não existe')
-         res.redirect('/admin/categorias')
-     })
+    }).catch((err) =>  {
+        req.flash('error_msg', 'Categotia não existe')
+        res.redirect('/admin/categorias')
+    })
 })
 
 router.post('/categorias/edit', (req,res) => {    
     Categoria.findOne({_id:req.body.id}).lean().then((categoria) => {
-        var erros = []
+        var erros = validaCategoria(req.body)
         
         
-    if (!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
-        erros.push({texto: 'Nome inválido'})
-    }
-    
-    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
-        erros.push({texto: 'Slug inválido'})
-    }
-
-    if (req.body.slug.length < 3) {
-        erros.push({
-            texto: 'O nome do Slug é muito curto, nescessário ter ao menos 3 digitos.'
-        })
-    }
-
-    if (/\s/g.test(req.body.slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter espaço vazio.'
-        })
-    }
-    
-    if (/[A-Z]/g.test(req.body.slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter letras maiúsculas.'
-        })
-    }
-    
-    if (/[0-9]/g.test(req.body.slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter numeros.'
-        })
-    }
-    
-    if (/[-_=+§~^´`:;/?.,><)(*&¬¨$#@!"'¹²³£¢|]/g.test(req.body.slug) == true) {
-        erros.push({
-            texto: 'O slug não pode conter caracteres especiais.'
-        })
-    }
-    
-    if (erros.length > 0 ) {
-        res.render('admin/editcategorias', {categoria:categoria, erros: erros})
-    }else{
-        Categoria.findOne({_id:req.body.id}).then((categoria) => {
-        categoria.nome = req.body.name
-        categoria.slug = req.body.slug
         
-        categoria.save().then(() => {
-            req.flash('success_msg','Categoria editada com sucesso')
+        if (erros.length > 0 ) {
+            res.render('admin/editcategorias', {categoria:categoria, erros: erros})
+        }else{
+            Categoria.findOne({_id:req.body.id}).then((categoria) => {
+                categoria.nome = req.body.name
+                categoria.slug = req.body.slug
+                
+                categoria.save().then(() => {
+                    req.flash('success_msg','Categoria editada com sucesso')
+                    res.redirect('/admin/categorias')
+                }).catch((err) => {
+                    req.flash('error_msg','Erro ao salvar edição da categoria')
+                    res.redirect('/admin/editcategorias')            
+                })
+            }
+            )}
+            
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro interno ao alterar a categoria.')
+            res.redirect('/admin/categorias')
+            
+        })
+        
+    })
+    
+    router.post('/categorias/deletar',(req,res) => {
+        Categoria.deleteOne({_id: req.body.id}).then(() => {
+            req.flash('success_msg','Categoria deletada com sucesso.')
             res.redirect('/admin/categorias')
         }).catch((err) => {
-            req.flash('error_msg','Erro ao salvar edição da categoria')
-            res.redirect('/admin/editcategorias')            
+            req.flash('error_msg','Erro ao deletar categoria.')
+            res.redirect('/admin/categorias')
+            
         })
-    }
-        )}
-
-    }).catch((err) => {
-        req.flash('error_msg', 'Erro interno ao alterar a categoria.')
-        res.redirect('/admin/categorias')
-
     })
-
-})
-
-router.post('/categorias/deletar',(req,res) => {
-    Categoria.deleteOne({_id: req.body.id}).then(() => {
-        req.flash('success_msg','Categoria deletada com sucesso.')
-        res.redirect('/admin/categorias')
-    }).catch((err) => {
-        req.flash('error_msg','Erro ao deletar categoria.')
-        res.redirect('/admin/categorias')
-
-    })
-})
-
-router.get('/postagens',(req,res) => {
-    Postagem.find().populate('categoria').sort({date:'desc'}).lean().then((postagens)=>{
+    
+    router.get('/postagens',(req,res) => {
+        Postagem.find().populate('categoria').sort({date:'desc'}).lean().then((postagens)=>{
         res.render('admin/postagens',{postagens:postagens})
     }).catch((err)=>{
         req.flash('error_msg','Erro ao listar a(s) postagens(s)')
         res.redirect('/admin/postagens')
     })
-
+    
 })
 
 router.get('/postagens/add',(req,res)=>{
@@ -191,17 +116,22 @@ router.get('/postagens/add',(req,res)=>{
 })
 
 router.post('/postagens/nova',(req,res)=>{
-    const novaPostagem = {
-        titulo: req.body.titulo,
-        slug: req.body.slug,
-        descricao: req.body.descricao,
-        conteudo: req.body.conteudo,
-        categoria: req.body.categoria
+  var erros = validaPostagem(req.body)  
+    if (erros.length > 0 ) {
+        res.render('admin/addpostagem', {erros: erros})
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            slug: req.body.slug,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria
+        }
+        new Postagem(novaPostagem).save().then(()=>{
+            req.flash('success_msg','Postagem criada com sucesso.')
+            res.redirect('/admin/postagens')
+        })
     }
-    new Postagem(novaPostagem).save().then(()=>{
-        req.flash('success_msg','Postagem criada com sucesso.')
-        res.redirect('/admin/postagens')
-    })
 })
 
 router.get('/postagens/edit/:id',(req,res)=>{
